16803fe220bbbfcf46471f2350643aa82b5287a7
Ayush Tomar <aayush.starr@gmail.com>
2023-09-09 07:51:57 +0530

    Tooltip changes

--- Bells/src/app/create-synthetic-data/create-synthetic-data.component.ts
+++ Bells/src/app/create-synthetic-data/create-synthetic-data.component.ts
@@ -44,11 +44,11 @@ export class CreateSyntheticDataComponent {
 
   transactions: TransactionType[] =
     [
-      new TransactionType('Sudden Deduction of huge amount', 1, false, 'big_amount_deduction'),
-      new TransactionType('High Volume Small Deductions', 2, false, 'high_volume_small_deduction'),
-      new TransactionType('Fake Merchants/Geo-Location Hopping/ Phishing website', 3, false, 'fake_merchant_false_location'),
-      new TransactionType('Multiple Retries before succes/Transactions in High-Risk Category (Casino, Child Ponography) ', 4, false, 'multiple_retry'),
-      new TransactionType('Money Laundering', 5, false, 'money_laundering')
+      new TransactionType('Sudden Deduction of huge amount', 1, false, 'big_amount_deduction', 'Upon gaining access to credit card/debit card, cracker spending huge amount on gift cards/purchases/transfer'),
+      new TransactionType('High Volume Small Deductions', 2, false, 'high_volume_small_deduction', 'Crackers doing payment in small amount but in high volumes.'),
+      new TransactionType('Fake Merchants/Geo-Location Hopping/ Phishing website', 3, false, 'fake_merchant_false_location', 'Payment done on suspected merchants, very different geographical locations or from phishing website'),
+      new TransactionType('Multiple Retries before succes/Transactions in High-Risk Category (Casino, Child Ponography) ', 4, false, 'multiple_retry', 'Multiple retries by the crackers before gaining access or transacting in high risk categories like child pornography'),
+      new TransactionType('Money Laundering', 5, false, 'money_laundering', 'Crackers doing money laundering across different channels using different techniques')
     ];
 
   allComplete: boolean = false;
@@ -83,12 +83,12 @@ export class TransactionType {
   isChecked: boolean = false;
   fileName: string;
   tooltip: string
-  constructor(name: string, id: number, isChecked = false, fileName: string) {
+  constructor(name: string, id: number, isChecked = false, fileName: string, tooltip: string = '') {
     this.name = name;
     this.id = id;
     this.isChecked = isChecked
     this.fileName = fileName
-    this.tooltip = 'sdgdsgsdg';
+    this.tooltip = tooltip;
   }
 }
 
